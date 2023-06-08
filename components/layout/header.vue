793476efd994e40e23301265ffb1684abbb94096
gimminjae <min356812@naver.com>
2023-06-08 22:41:52 +0900

    feat: create default, header, footer, index.vue

---
+++ components/layout/header.vue
@@ -0,0 +1,7 @@
+<template>
+    <header>
+        header
+    </header>
+</template>
+<script setup>
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <LayoutHeader></LayoutHeader>
+        default.vue
+        <LayoutFooter></LayoutFooter>
+    </div>
+</template>
+<script setup>
+</script>
