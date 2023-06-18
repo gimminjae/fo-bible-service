cd545386e6e3a857474c15ca69237fcea771014e
gimminjae <min356812@naver.com>
2023-06-18 17:09:19 +0900

    design: setting page

--- pages/setting/index.vue
+++ pages/setting/index.vue
@@ -1,6 +1,8 @@
 <template>
     <div>
-        setting page
+        <div class="navbar bg-base-100 flex justify-around fixed top-0">
+            <h1>환경설정</h1>
+        </div>
     </div>
 </template>
 <script>
