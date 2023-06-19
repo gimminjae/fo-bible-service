f5bfa02c56ec93d91b6a1227576234a32954c79c
gimminjae <min356812@naver.com>
2023-06-19 16:31:26 +0900

    feat: 성경찾기에서 뒤로가기 구현

--- pages/bible/search.vue
+++ pages/bible/search.vue
@@ -1,12 +1,23 @@
 <template>
     <div class="mx-3">
+        <div class="navbar bg-base-100 fixed top-0">
+            <div class="flex-none">
+                <button class="btn btn-square btn-ghost" @click="useRouter().back()">
+                    {{ backChar }}
+                </button>
+            </div>
+            <div class="flex-1">
+                <a class="btn btn-ghost normal-case text-xl">성경찾기</a>
+            </div>
+        </div>
         <div v-for="bible in bibleList" tabindex="0" class="collapse collapse-arrow border border-base-300 bg-base-200">
             <input type="checkbox" />
             <div class="collapse-title text-xl font-medium">
-                {{ bible.bookName}}
+                {{ bible.bookName }}
             </div>
             <div class="collapse-content grid grid-cols-5 gap-4">
-                <button v-for="chapter in bible.chapterCount" class="btn" @click="goBible(bible, chapter)">{{ chapter }}</button>
+                <button v-for="chapter in bible.chapterCount" class="btn" @click="goBible(bible, chapter)">{{ chapter
+                }}</button>
             </div>
         </div>
     </div>
@@ -15,6 +26,7 @@
 import bibles from '~/bible/objects/bibles'
 import cookieUtil from '~/composables/cookie';
 
+const backChar = ref('<<')
 const router = useRouter()
 const bibleList = bibles.getKoBible()
 
