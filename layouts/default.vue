4b840497ada998beee4f80e22cc22ec71ae8fdb5
gimminjae <min356812@naver.com>
2023-06-19 16:40:12 +0900

    design: apply fontawesome / change footermenu icon

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
         <LayoutHeader/>
         <NuxtPage />
         <LayoutFooter/>
