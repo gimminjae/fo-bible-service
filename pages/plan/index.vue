6c55994e0a05632c719f8baf40a24ba8fa94a28f
gimminjae <min356812@naver.com>
2023-07-21 20:19:49 +0900

    feat: 읽기표 목록 생성

--- pages/plan/index.vue
+++ pages/plan/index.vue
@@ -3,12 +3,24 @@
         <div class="navbar bg-base-100 flex justify-around sticky top-0">
             <h1>성경읽기표</h1>
         </div>
-        <div class="flex justify-end mr-3">
+        <div class="flex justify-end mr-3 mb-3">
             <NuxtLink class="btn btn-primary btn-sm" to="/plan/new">읽기표 만들기</NuxtLink>
             <!-- Open the modal using ID.showModal() method -->
         </div>
-        <div>
-            {{ planList }}
+        <div class="flex justify-center flex-wrap gap-3 overflow-auto">
+            <div class="card w-96 bg-neutral text-neutral-content" v-for="plan in planList">
+                <div class="card-body">
+                    <h2 class="card-title">
+                        {{ plan.planName }}
+                        <div class="badge badge-secondary">{{ plan.goalPercent }} %</div>
+                    </h2>
+                    <p>구약 {{ plan.oldGoalCount }}독! 신약 {{ plan.newGoalCount }}독!</p>
+                    <div class="card-actions justify-end">
+                        <div class="badge badge-outline">시작일 {{ plan.startDate }}</div>
+                        <div class="badge badge-outline">종료일 {{ plan.endDate }}</div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
