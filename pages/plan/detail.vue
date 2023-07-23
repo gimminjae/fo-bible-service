53a73f0c60bd2a2dfe3eaea4d190c467d7c012a7
gimminjae <min356812@naver.com>
2023-07-23 15:42:07 +0900

    chore: 주석 제거

--- pages/plan/detail.vue
+++ pages/plan/detail.vue
@@ -3,17 +3,6 @@
       <div class="navbar bg-base-100 flex justify-around sticky top-0">
           <h1>{{ planInfo.planName }}</h1>
       </div>
-<!--      <div class="flex" v-for="status in planInfo.goalStatus">-->
-<!--          <div class="text-center whitespace-nowrap align-middle">{{ status.bookName }}</div>-->
-<!--          <div class="card-body">-->
-<!--              <div class="grid grid-cols-10 gap-3">-->
-<!--                  <div class="indicator" v-for="(verse, index) in status.verseStatus">-->
-<!--                      &lt;!&ndash;                  <span class="indicator-item badge badge-xs badge-ghost">{{ verse }}</span>&ndash;&gt;-->
-<!--                      <button class="btn btn-xs">{{ index + 1 }}</button>-->
-<!--                  </div>-->
-<!--              </div>-->
-<!--          </div>-->
-<!--      </div>-->
       <div class="tabs">
           <a :class="`tab tab-bordered ${ tab === 'Main' ? 'tab-active' : ''}`" @click="changeTab('Main')">Main</a>
           <a :class="`tab tab-bordered ${ tab === 'old' ? 'tab-active' : ''}`" @click="changeTab('old')">구약</a>
@@ -24,7 +13,6 @@
           <div v-if="tab === 'old' && status.testament" class="stat">
               <div class="grid grid-cols-10 gap-x-3 gap-y-1">
                   <div class="indicator" v-for="(verse, index) in status.verseStatus">
-                      <!--                  <span class="indicator-item badge badge-xs badge-ghost">{{ verse }}</span>-->
                       <button class="btn btn-xs">{{ index + 1 }}</button>
                   </div>
               </div>
@@ -34,22 +22,11 @@
               <div class="grid grid-cols-10 gap-3">
                   <div class="indicator" v-for="(verse, index) in status.verseStatus">
                       <!--                  <span class="indicator-item badge badge-xs badge-ghost">{{ verse }}</span>-->
-                      <button class="btn btn-xs">{{ index + 1 }}</button>
+                      <button :class="`btn btn-xs ${verse > 0 ? 'btn-primary' : ''}`">{{ index + 1 }}</button>
                   </div>
               </div>
           </div>
       </div>
-<!--      <div class="card card-side" v-for="status in planInfo.goalStatus">-->
-<!--          <figure class="text-center">{{ status.bookName }}</figure>-->
-<!--          <div class="card-body grid grid-cols-10">-->
-<!--              <div class="indicator mx-5 my-1" v-for="(verse, index) in status.verseStatus">-->
-<!--                  <span class="indicator-item badge badge-xs badge-ghost">{{ verse }}</span>-->
-<!--                  <button class="btn btn-xs">{{ index + 1 }}</button>-->
-<!--              </div>-->
-<!--              <div class="card-actions justify-end">-->
-<!--              </div>-->
-<!--          </div>-->
-<!--      </div>-->
   </div>
 </template>
 <script setup>
